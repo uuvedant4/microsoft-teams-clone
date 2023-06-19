0d4f40302a278d3884cf960fa68b55f7abf8a471
vedant <ursvedantyetekar@gmail.com>
2023-06-19 22:52:13 +0530

    sidebar component created

--- src/components/common/sidebar/index.js
+++ src/components/common/sidebar/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import { SidebarBottom, SidebarData, SidebarMore } from "../../../data/sidebar";
+import SidebarOption from "./sidebar-option";
+import "./sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const topOptions = SidebarData;
+  const more = SidebarMore;
+  const bottomOptions = SidebarBottom;
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar-top">
+        <div>
+          {topOptions.map((option) => {
+            return (
+              <SidebarOption
+                option={option}
+                isActive={option.name === "Calls" ? true : false}
+              />
+            );
+          })}
+        </div>
+        <div>
+          <SidebarOption option={more} />
+        </div>
+      </div>
+      <div className="sidebar-bottom">
+        {bottomOptions.map((option) => {
+          return <SidebarOption option={option} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

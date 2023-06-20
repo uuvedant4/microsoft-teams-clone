6f44b450fa2de93b8ab6092c8b322434b3687875
vedant <ursvedantyetekar@gmail.com>
2023-06-20 16:20:39 +0530

    completed contacts section

--- src/data/calls.js
+++ src/data/calls.js
@@ -26,18 +26,63 @@ export const DialerList = [
     id: 1,
     name: "Vedant Yetekar",
     type: "Software Engineer",
-    avatar: "https://cdn-icons-png.flaticon.com/128/6219/6219219.png",
+    avatar: "https://cdn-icons-png.flaticon.com/128/3271/3271488.png",
   },
   {
     id: 2,
     name: "Nikunj Gujarathi",
     type: "Automobile Engineer",
-    avatar: "https://cdn-icons-png.flaticon.com/128/2825/2825132.png",
+    avatar: "https://cdn-icons-png.flaticon.com/128/9562/9562869.png",
   },
   {
     id: 3,
     name: "Shriharsh Swami",
     type: "Mechanical Engineer",
-    avatar: "https://cdn-icons-png.flaticon.com/128/7453/7453898.png",
+    avatar: "https://cdn-icons-png.flaticon.com/128/3146/3146501.png",
+  },
+];
+
+export const CallsContact = [
+  {
+    id: 1,
+    name: "Anish Wanare",
+    type: "Software Developer",
+    avatar: "https://cdn-icons-png.flaticon.com/128/5757/5757045.png",
+  },
+  {
+    id: 2,
+    name: "Shrish Dollin",
+    type: "Software Engineer",
+    avatar: "https://cdn-icons-png.flaticon.com/128/6010/6010054.png",
+  },
+  {
+    id: 3,
+    name: "Chaitanya Apsunde",
+    type: "Student",
+    avatar: "https://cdn-icons-png.flaticon.com/128/3429/3429417.png",
+  },
+  {
+    id: 4,
+    name: "Ashnit Jakkal",
+    type: "Robotics Engineer",
+    avatar: "https://cdn-icons-png.flaticon.com/128/4691/4691497.png",
+  },
+  {
+    id: 5,
+    name: "Harshvardhan Chaudhary",
+    type: "Student",
+    avatar: "https://cdn-icons-png.flaticon.com/128/2784/2784403.png",
+  },
+  {
+    id: 6,
+    name: "Makarand More",
+    type: "Mechanical Design Engineer",
+    avatar: "https://cdn-icons-png.flaticon.com/128/3597/3597830.png",
+  },
+  {
+    id: 7,
+    name: "Devashish Pawar",
+    type: "Automobile Engineer",
+    avatar: "https://cdn-icons-png.flaticon.com/128/1839/1839365.png",
   },
 ];
